23a1d1c2fb576722115dd3223c55058b406186f3
unknown <mehedi.github@gmail.com>
2022-07-26 09:53:32 +0600

    model classes added

--- app/src/main/java/com/example/galleryappdemo/network/PhotoServiceApi.java
+++ app/src/main/java/com/example/galleryappdemo/network/PhotoServiceApi.java
@@ -1,10 +1,13 @@
 package com.example.galleryappdemo.network;
 
+import com.example.galleryappdemo.models.PhotoModel;
+
+import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface PhotoServiceApi {
 
-   // @GET("photos")
-    //Call<PhotoModel> getCurrentData();
+    @GET("photos")
+    Call<PhotoModel> getCurrentData();
 
 }
